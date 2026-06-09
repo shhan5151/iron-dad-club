abb1a6b0907c666d7ca8c85a6e7d956f954f99d9
shhan5151 <hannah@aiposthub.com>
2026-06-09 12:52:55 +0800

    Add configurable site image slots

---
+++ src/lib/media.ts
@@ -0,0 +1,15 @@
+export type SiteImages = {
+  beforeLoginBackground: string;
+  beforeLoginProfile: string;
+  afterLoginMemoryCollage: string;
+  afterLoginCard: string;
+  afterLoginUltrasound: string;
+};
+
+export const defaultSiteImages: SiteImages = {
+  beforeLoginBackground: "/photos/davin-bike-coast.jpg",
+  beforeLoginProfile: "/photos/davin-profile.jpg",
+  afterLoginMemoryCollage: "/photos/memory-collage.png",
+  afterLoginCard: "/photos/card-photo.JPG",
+  afterLoginUltrasound: "/photos/elara-ultrasound.jpg",
+};
